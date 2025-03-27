5bcb968e9d42c036a8a910a65042eac2062b5623
Brett Fowler <b16fowler@gmail.com>
2025-03-27 12:11:05 -0400

    Testing button and array map functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
@@ -7,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <ShoppingList />
+          <MyButton />
         </p>
         <a
           className="App-link"
@@ -19,6 +21,50 @@ function App() {
         </a>
       </header>
     </div>
+  );
+}
+
+function MyButton() {
+  function handleClick() {
+    alert("I've been clicked!")
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  )
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
   );
 }
 
